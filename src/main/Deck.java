d96ad1b7a10f3607df5d81dae0f4816359ae0234
Asher <asherbnw8@gmail.com>
2024-06-15 11:54:52 -0400

    added game logic

--- src/main/Deck.java
+++ src/main/Deck.java
@@ -8,11 +8,15 @@ public class Deck {
     private List<Card> cards = new ArrayList<>();
 
     public Deck() {
-        Suit[] suits = {Suit.HEARTS, Suit.DIAMONDS, Suit.SPADES, Suit.CLUBS};
-//        todo: make rank enum
-        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
-        int[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
+        refreshDeck();
+    }
 
+    // todo: this method needs testing
+    public void refreshDeck() {
+        Suit[] suits = {Suit.HEARTS, Suit.DIAMONDS, Suit.SPADES, Suit.CLUBS};
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        //        todo: make rank enum
+        int[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
         for (Suit suit : suits) {
             for (int i = 0; i < ranks.length; i++) {
                 cards.add(new Card(suit, ranks[i], values[i]));
@@ -26,18 +30,19 @@ public class Deck {
     }
 
     public Card draw() {
-        if (!cards.isEmpty()) {    // if cards list (deck) has at least 1 card in it...
-            return cards.remove(0);
+        if (cards.isEmpty()) {    // if cards list (deck) has at least 1 card in it...
+            refreshDeck();
         }
-        return null;
+        return cards.remove(0);
     }
     public Card[] draw(int numOfCards) {
         int i;
         Card[] cardsDrawn = new Card[numOfCards];
         for (i = 0; i < numOfCards; i++){
-            if (!cards.isEmpty()) {    // if cards list (deck) has at least 1 card in it...
-                cardsDrawn[i] = cards.remove(i);
+            if (cards.isEmpty()) {    // if cards list (deck) has at least 1 card in it...
+                refreshDeck();
             }
+            cardsDrawn[i] = cards.remove(i);
         }
         return cardsDrawn;
     }
--- src/main/Main.java
+++ src/main/Main.java
@@ -6,17 +6,27 @@ import org.w3c.dom.ls.LSOutput;
 import java.io.InputStream;
 import java.util.Scanner;
 
+// todo: give option of playing again
 public class Main {
     public static void main(String[] args) {
         Deck deck = new Deck();
         Hand player1 = new HandImpl();
-        makePlayer(player1, deck);
+        Hand dealer = new HandImpl();
+        dealHand(player1, deck);     //deals a player 2 cards
+        dealHand(dealer, deck);
+
+        Card faceUpCard = dealer.getCards().get(0);
+
         System.out.println("total value: " + player1.getTotalValue());
         System.out.println("hand: " + player1.getCards());
+        System.out.println("dealer: " + faceUpCard);
+//        System.out.println("dealer total cards: " + dealer.getCards());
+//        System.out.println("dealer total value: " + dealer.getTotalValue());
+        System.out.println();
 
         Scanner sc = new Scanner(System.in);
-
         boolean loop = true;
+        boolean busted = false;
 
         while (loop) {
             System.out.println("HIT (h) or STAND (s)?");
@@ -27,44 +37,60 @@ public class Main {
                 System.out.println("you chose to hit and drew a " + cardDrawn + "!");
                 System.out.println("total value: " + player1.getTotalValue());
                 System.out.println("hand: " + player1.getCards());
-                System.out.println();
-
-                /** NOTE: using StringBuilder is more efficient because it is mutable
-                 * Strings, however, are immutable,
-                 * so each '+' results in a new string creation
-                 *
-                 * StringBuilder sb = new StringBuilder();
-                 * sb.append("you chose to hit and drew a ").append(cardDrawn).append("!\n");
-                 * sb.append("total value: ").append(player1.getTotalValue()).append("\n");
-                 * sb.append("hand: ").append(player1.getCards()).append("\n");
-                 * sb.append("\n");
-                 * System.out.println(sb.toString());
-                 * */
-            }
-
-//            if (choice.equals("s")) {
-            else if (choice.equals("s")) {
+                if (player1.getTotalValue() > 21) {
+                    System.out.println("you busted, you lose!");
+                    loop = false;
+                    busted = true;
+                } else {
+                    System.out.println();
+                }
+            } else if (choice.equals("s")) {
                 System.out.println("you chose to stand!");
                 System.out.println("total value: " + player1.getTotalValue());
                 System.out.println("hand: " + player1.getCards());
                 System.out.println();
                 loop = false;
-            }
-
-//            if (!choice.equals("s") && !choice.equals("h")) {
-            else {
+            } else {
                 System.out.println("invalid response...");
             }
         }
 
+        if (!busted) {
+            System.out.println("dealer reveals hidden card: " + dealer.getCards());
+            System.out.println("dealer total value: " + dealer.getTotalValue());
+            while (dealer.getTotalValue() < 17) {
+                Card cardDrawn = deck.draw();
+                dealer.addCard(cardDrawn);
+                System.out.println("dealer draws a " + cardDrawn);
+                System.out.println("dealer total value: " + dealer.getTotalValue());
+                if (dealer.getTotalValue() > 21) {
+                    System.out.println("dealer busts!");
+                }
+            }
+            int dealerFinalValue = dealer.getTotalValue();
+            int playerFinalValue = player1.getTotalValue();
 
-//        player1.addCard(deck.draw());
+            if (dealerFinalValue > playerFinalValue && dealerFinalValue <= 21) {
+                System.out.println();
+                System.out.println("you lose!");
+            }
 
-//        main.HandImpl player2 = new main.HandImpl();
+            else if (dealerFinalValue == playerFinalValue && dealerFinalValue <= 21) {
+                System.out.println();
+                System.out.println("it's a draw!");
+            }
 
+            else {
+                System.out.println();
+                System.out.println("you win!");
+            }
+        }
     }
-    public static void makePlayer(Hand player, Deck deck){
+    public static void dealHand(Hand player, Deck deck){
         player.addCards(deck.draw(2));
+    }
+}
+
 
 
 //        Card ace1 = new Card("hearts", "Ace", 11);
@@ -72,6 +98,15 @@ public class Main {
 //        player.addCard(ace1);
 //        main.Card ace2 = new main.Card("diamonds", "Ace", 11);
 //        player.addCard(ace2);
-    }
 
-}
+/** NOTE: using StringBuilder is more efficient because it is mutable
+ * Strings, however, are immutable,
+ * so each '+' results in a new string creation
+ *
+ * StringBuilder sb = new StringBuilder();
+ * sb.append("you chose to hit and drew a ").append(cardDrawn).append("!\n");
+ * sb.append("total value: ").append(player1.getTotalValue()).append("\n");
+ * sb.append("hand: ").append(player1.getCards()).append("\n");
+ * sb.append("\n");
+ * System.out.println(sb.toString());
+ * */
